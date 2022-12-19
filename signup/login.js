2ec5508df83253b0cc1e0e9ef02ace370cbf9e0d
Gurmeet Singh <gurmeetsingh6175@gmail.com>
2022-12-19 13:52:04 +0530

    login logout added

---
+++ signup/login.js
@@ -0,0 +1,25 @@
+let signup_arr = JSON.parse(localStorage.getItem("signup"));
+let login = () => {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("pass").value;
+
+  let user_data = { email, password };
+  if (checkUser(user_data) === true) {
+    alert("You are Successfully login");
+    window.location.href = "../index.html";
+  } else {
+    alert("Your given credential does not match");
+  }
+  document.getElementById("email").value = "";
+  document.getElementById("pass").value = "";
+};
+let checkUser = (e) => {
+  let filtered = signup_arr.filter((el) => {
+    return e.email === el.email && e.password === el.password;
+  });
+  if (filtered.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
---
+++ signup/signup.js
@@ -0,0 +1,30 @@
+// let url="https://masai-api-mocker.herokuapp.com"
+let data_arr = JSON.parse(localStorage.getItem("signup")) || [];
+
+let signup = () => {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("pass").value;
+
+  let data_obj = { name, email, password };
+  if (checkUser(data_obj) === true) {
+    data_arr.push(data_obj);
+    localStorage.setItem("signup", JSON.stringify(data_arr));
+    alert("You are Successfully Signup");
+  } else {
+    alert("Already Signup");
+  }
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("pass").value = "";
+};
+let checkUser = (e) => {
+  let filtered = data_arr.filter((el) => {
+    return e.email === el.email;
+  });
+  if (filtered.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
